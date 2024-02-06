64eb38fea299fd223e7a07e5620bc7956fa09afc
jcat1504 <jyin978@hotmail.com>
2024-02-05 20:19:19 -0500

    navbar roughdraft

---
+++ client/src/components/Nav.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Nav = () => {
+    return (
+        <nav className='navbar'>
+            <h3>My Task List</h3>
+        </nav>
+    );
+};
+
+export default Nav;
